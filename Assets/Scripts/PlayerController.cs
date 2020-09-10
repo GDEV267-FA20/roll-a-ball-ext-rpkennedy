2496d8bca819a7e558e1de3a25321cce23978d45
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-09 20:12:08 -0400

    Rollin_v1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Create public variables for player speed, and for the Text UI game objects
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private float movementX;
+    private float movementY;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+        movementY = v.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+}
+
